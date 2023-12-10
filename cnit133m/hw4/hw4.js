dd8ed793f9cd7fc7eed18131e7fb9ebcf90655b2
kerryng79 <46254748+kerryng79@users.noreply.github.com>
2023-12-10 11:45:52 -0800

    Add files via upload

---
+++ cnit133m/hw4/hw4.js
@@ -0,0 +1,57 @@
+if (navigator.serviceWorker) {
+  window.addEventListener('load', () => {
+   navigator.serviceWorker.register('./service-worker.js')
+      .then(registration => console.log('SW registered'))
+    .catch(err => console.log(`SW not registered - Error: ${err}`))
+  })
+} else {
+  console.log('Service Worker is not supported in this browser.')
+}
+
+// function to change background image
+const changeBackground = () => {
+  var select = document.getElementById("backgroundSelector");
+  if(select.options[select.selectedIndex].value !== "") {
+    var selectedImage = select.options[select.selectedIndex].value;
+    document.body.style.backgroundImage = `url('${selectedImage}')`;
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.backgroundSize = "cover";
+  } else {
+    document.body.style.backgroundImage = "";
+    document.body.style.backgroundColor = "#ffffff";
+  }
+}
+
+
+/* extra credit of local storage starts here */
+
+const saveName = () => {
+      var nameInput = document.getElementById('name');
+      var name = nameInput.value;
+
+      if (name.trim() !== '') {
+          localStorage.setItem('username', name);
+
+          displayStoredName();
+      }
+  };
+
+ const displayStoredName = () => {
+      var storedName = localStorage.getItem('username');
+      if(storedName) {
+        document.getElementById("display").innerHTML = `Hello, ${storedName}, glad to see you back!`;
+      } else {
+        document.getElementById("display").innerHTML = '';
+      }
+  };
+
+
+  document.getElementById('name').addEventListener('blur', () => {
+    saveName();
+  });
+  
+
+  // Initial display when the page loads
+  displayStoredName();
+
+  /* extra credit of local storage ends here */
